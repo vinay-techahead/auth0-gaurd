1104aaf69dff50ae1c91bd53bc468d01ecb76dcc
Vinay <vinay.kumar@techaheadcorp.com>
2025-11-13 18:29:26 +0530

    fix: enable redis cluster support

--- src/redis.service.ts
+++ src/redis.service.ts
@@ -1,44 +1,66 @@
 import { Injectable, OnModuleDestroy } from "@nestjs/common";
-import Redis from "ioredis";
+import Redis, {
+  Cluster,
+  ClusterNode,
+  ClusterOptions,
+  Redis as RedisType,
+} from "ioredis";
 import { getEnv } from "./env";
-interface RedisConfig {
-  host: string;
-  port: number;
-  db: number;
-  username?: string;
-  password?: string;
-  maxRetriesPerRequest: number;
-  connectTimeout: number;
-  lazyConnect: boolean;
-  tls?: any;
-}
 
 @Injectable()
 export class RedisService implements OnModuleDestroy {
-  private client: Redis;
+  private client: RedisType | Cluster;
 
   constructor() {
-    const config: RedisConfig = {
-      host: getEnv("REDIS_HOST") || "localhost",
-      port: Number(getEnv("REDIS_PORT") || 6379),
+    const host = getEnv("REDIS_HOST") || "localhost";
+
+    const port = Number(getEnv("REDIS_PORT") || 6379);
+
+    const redisOptions = {
       db: Number(getEnv("REDIS_DB") || 0),
-      password: getEnv("REDIS_PASSWORD"),
+      password: getEnv("REDIS_PASSWORD") || undefined,
       maxRetriesPerRequest: 3,
       connectTimeout: 10000,
       lazyConnect: true,
+      tls: getEnv("REDIS_TLS") ? { rejectUnauthorized: false } : undefined,
     };
 
-    if (getEnv("REDIS_PASSWORD")) {
-      config.password = getEnv("REDIS_PASSWORD");
-    }
-    if (getEnv("REDIS_TLS")) {
-      config.tls = { rejectUnauthorized: false };
-    }
-    this.client = new Redis(config);
+    if (getEnv("REDIS_CLUSTER") === "true") {
+      const nodes: ClusterNode[] = [
+        {
+          host,
+          port,
+        },
+      ];
 
-    this.client.on("error", (err: Error) => {
-      console.error("Redis Client Error:", err);
-    });
+      const clusterOptions: ClusterOptions = {
+        redisOptions,
+      };
+
+      this.client = new Cluster(nodes, clusterOptions);
+
+      this.client.on("connect", () => {
+        console.log("[Redis Cluster] Connected");
+      });
+
+      this.client.on("error", (err: Error) => {
+        console.error("[Redis Cluster Error]:", err.message);
+      });
+    } else {
+      this.client = new Redis({
+        host: host,
+        port,
+        ...redisOptions,
+      });
+
+      this.client.on("connect", () => {
+        console.log("[Redis] Connected");
+      });
+
+      this.client.on("error", (err: Error) => {
+        console.error("[Redis Error]:", err.message);
+      });
+    }
   }
 
   async getUserData(userSub: string): Promise<any | null> {
