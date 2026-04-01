c10c2294dfabb6c31cf67189973e0b2ad67a283f
Vinay <vinay.kumar@techaheadcorp.com>
2026-04-01 13:02:40 +0530

    update package

--- src/auth.guard.ts
+++ src/auth.guard.ts
@@ -77,6 +77,7 @@ export class AuthGuard implements CanActivate {
           allowedStores: userData.storeIds,
         }),
       };
+      console.log(request.user);
     } else {
       // If no Redis data found, throw an error
       throw new UnauthorizedException("No active login session found");
